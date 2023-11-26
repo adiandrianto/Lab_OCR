ae2a360880fe0e47eec950e42fa7f4d9cf7867f8
Adi Andrianto <adiandrianto@gmail.com>
2023-11-26 23:03:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -42,39 +42,19 @@ def process_pdf_files(path):
     
     return df_to_excel(combined_df_list,1)
 
-def login():
-    st.title("Login")
+st.title("Lab Result PDF to Excel")
+uploaded_files = st.file_uploader("Upload PDF file(s)", type="pdf", accept_multiple_files=True)
 
-    # Retrieve username and password from st.secrets
-    username = st.secrets["login"]["username"]
-    password = st.secrets["login"]["password"]
+if st.button("Convert to Excel"):
+    if uploaded_files:
+        path = uploaded_path(uploaded_files)
+        st.write("Processing, please wait...")
+        output = process_pdf_files(path)
+        st.write("Processing complete!")
 
-    # Create input fields for username and password
-    user_input = st.text_input("Username")
-    pass_input = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        if user_input == username and pass_input == password:
-            return True
-        else:
-            st.error("Invalid username or password")
-    return False
-
-if __name__ == "__main__":
-    if login():
-        st.title("Lab Result PDF to Excel")
-        uploaded_files = st.file_uploader("Upload PDF file(s)", type="pdf", accept_multiple_files=True)
-
-        if st.button("Convert to Excel"):
-            if uploaded_files:
-                path = uploaded_path(uploaded_files)
-                st.write("Processing, please wait...")
-                output = process_pdf_files(path)
-                st.write("Processing complete!")
-
-            # Download button for the processed file
-                with open(output, "rb") as file:
-                    file_bytes = file.read()
-                    st.download_button(label="Download Excel File", data=file_bytes, file_name='hasil.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            else:
-                st.warning("Please upload PDF files.")
+    # Download button for the processed file
+        with open(output, "rb") as file:
+            file_bytes = file.read()
+            st.download_button(label="Download Excel File", data=file_bytes, file_name='hasil.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    else:
+        st.warning("Please upload PDF files.")
